05f4a4320eab5f701b2e71ca8217d9a93dbee730
jiloysss <janlloydangeles@gmail.com>
2021-09-26 17:35:39 +0800

    Update

--- spreads/doc_events/sales_invoice.py
+++ spreads/doc_events/sales_invoice.py
@@ -25,11 +25,8 @@ def get_items(doc):
         }
         if doc.items[0].sales_order:
             so = frappe.db.sql(""" SELECT * FROM `tabSales Order` WHERE name=%s""",(doc.items[0].sales_order),as_dict=1 )
-            cost_center = so[0].project_code if len(so) > 0 and so[0].existing_project_code else get_cost_center(so[0].new_project_code) if len(so) > 0 and not so[0].existing_project_code else ""
+            cost_center = so[0].project_code if len(so) > 0 and so[0].project_code else ""
         if cost_center:
             obj['cost_center'] = cost_center
         items.append(obj)
     return items
-
-def get_cost_center(new_project_code):
-    return frappe.db.sql(""" SELECT * FROM `tabCost Center` WHERE cost_center_name=%s """, new_project_code, as_dict=1)[0].name
