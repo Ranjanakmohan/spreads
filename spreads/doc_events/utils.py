6be4835f8a09144dc1fa0f4f557ab0ed91777d46
jiloysss <janlloydangeles@gmail.com>
2021-09-28 16:55:50 +0800

    Update

--- spreads/doc_events/utils.py
+++ spreads/doc_events/utils.py
@@ -6,10 +6,3 @@ def validate_raw_material(doc, method):
     for i in doc.items:
         if i.is_service_item and not doc.raw_material:
             frappe.throw("Raw Material is Mandatory")
-
-
-def check_raw_material(doc, i):
-    for ii in doc.raw_material:
-        if ii.service_item == i.item_code:
-            return True
-    return False
