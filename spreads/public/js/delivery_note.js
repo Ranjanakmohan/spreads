eafce5de75d7c1a90c8f1743390dd2339456a45a
jiloysss <janlloydangeles@gmail.com>
2021-10-04 19:33:36 +0800

    Update

--- spreads/public/js/delivery_note.js
+++ spreads/public/js/delivery_note.js
@@ -1,12 +1,4 @@
-cur_frm.cscript.onload = function () {
-    if(cur_frm.doc.is_return){
-        $.each(cur_frm.doc.raw_material, function(i, row) {
-			row.qty_raw_material = 0 - row.qty_raw_material
-            row.amount = row.qty_raw_material * row.rate
-            cur_frm.refresh_field("raw_material")
-		})
-    }
-}
+
 cur_frm.cscript.onload_post_render = function () {
     cur_frm.set_query("item_code_raw_material", "raw_material", () => {
             return {
