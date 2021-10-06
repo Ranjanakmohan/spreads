51a459d2991de21f82d355661586c77a3e69c95b
jiloysss <janlloydangeles@gmail.com>
2021-10-06 13:32:40 +0800

    Update

--- spreads/public/js/item.js
+++ spreads/public/js/item.js
@@ -8,3 +8,22 @@ cur_frm.cscript.assembled_item = function(){
     cur_frm.doc.is_stock_item = false
     cur_frm.refresh_field("is_stock_item")
 }
+
+cur_frm.cscript.item_group = function () {
+    if(cur_frm,doc.item_group){
+        frappe.db.get_doc('Item Group', cur_frm.doc.item_group)
+            .then(doc => {
+                cur_frm.doc.is_stock_item = doc.is_maintain_stock
+                cur_frm.doc.is_sales_item = doc.is_sales_item
+                cur_frm.doc.is_purchase_item = doc.is_purchase_item
+                cur_frm.doc.is_fixed_asset = doc.is_fixed_asset
+                cur_frm.doc.is_service_item = doc.is_service_item
+                cur_frm.refresh_field("is_stock_item")
+                cur_frm.refresh_field("is_sales_item")
+                cur_frm.refresh_field("is_purchase_item")
+                cur_frm.refresh_field("is_fixed_asset")
+                cur_frm.refresh_field("is_service_item")
+            })
+    }
+
+}
