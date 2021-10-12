a07deb4fdf36e9b616be51f5a6358280369d1bb6
jiloysss <janlloydangeles@gmail.com>
2021-10-12 12:54:28 +0800

    Update

--- spreads/doc_events/item_group.py
+++ spreads/doc_events/item_group.py
@@ -1,7 +1,7 @@
 import frappe
 
 def validate_item_group(doc, method):
-    if doc.parent_item_group != "All Item Group" and not doc.is_group:
+    if doc.parent_item_group != "All Item Groups" and not doc.is_group:
 
         doc.is_maintain_stock = frappe.db.get_value('Item Group', doc.parent_item_group, "is_maintain_stock")
         doc.is_sales_item = frappe.db.get_value('Item Group', doc.parent_item_group, "is_sales_item")
