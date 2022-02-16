934e9c174ee5d39b93ba8b8d1ef2f73b4e744a5a
jiloysss <janlloydangeles@gmail.com>
2022-02-16 11:45:24 +0800

    Update

--- spreads/doc_events/sales_order.py
+++ spreads/doc_events/sales_order.py
@@ -1,6 +1,94 @@
-import frappe
+import frappe, json
 from frappe.model.mapper import get_mapped_doc
-from frappe.utils import flt
+from spreads.doc_events.account_controller import update_child_qty_rate
+
+
+@frappe.whitelist()
+def update_raw_material(parent_doctype,raw_material,parent_doctype_name,child_docname):
+    print("UPDAAAAAAAAAAAAAAAAAAAAAAAATE")
+    data = []
+    raw_materials = json.loads(raw_material)
+    items = frappe.get_doc("Sales Order", parent_doctype_name).items
+    frappe.db.sql(""" DELETE FROm `tabRaw Material` WHERE parent= %s""",parent_doctype_name)
+    frappe.db.commit()
+    for i in items:
+        total = 0
+        for x in raw_materials:
+
+            if x['service_item']:
+                if i.item_code == x['service_item']:
+                    total += (x['qty'] * x['rate'])
+        data.append({
+            "docname": i.name,
+            "name": i.name,
+            "item_code": i.item_code,
+            "delivery_date": i.delivery_date,
+            "conversion_factor": i.conversion_factor,
+            "qty": i.qty,
+            "rate": total,
+            "uom": i.uom,
+            "idx": i.idx,
+            "__checked": 1
+        })
+    update_child_qty_rate(parent_doctype,data, parent_doctype_name)
+    create_raw_material(raw_materials, parent_doctype_name)
+
+def create_raw_material(items,parent_doctype_name):
+    total_raw_material_expense = total_raw_material_buying_expense = 0
+    for item in items:
+        total_raw_material_expense += (item['qty'] * item['rate'])
+        buying_price = item['buying_price'] if 'buying_price' in item else 0
+        total_raw_material_buying_expense += (item['qty'] * buying_price)
+        item['parent'] = parent_doctype_name
+        item['parenttype'] = "Sales Order"
+        item['parentfield'] = "raw_material"
+        item['doctype'] = "Raw Material"
+        frappe.get_doc(item).insert()
+    raw_material_profit = total_raw_material_expense - total_raw_material_buying_expense
+    frappe.db.sql(""" UPDATE `tabSales Order` SET raw_material_profit=%s, total_raw_material_expense=%s, total_raw_material_buying_expense=%s WHERE name=%s""",
+                  (raw_material_profit, total_raw_material_expense, total_raw_material_buying_expense, parent_doctype_name))
+    frappe.db.commit()
+def update_so(doc, method):
+    data = []
+    for i in doc.items:
+        total = 0
+        for x in doc.raw_material:
+            if x.service_item:
+                if i.item_code == x.service_item:
+                    x.amount += total
+        data.append({
+            "docname": i.name,
+            "name": i.name,
+            "item_code": i.item_code,
+            "delivery_date": i.delivery_date,
+            "conversion_factor": i.conversion_factor,
+            "qty": i.qty,
+            "rate": total,
+            "uom": i.uom,
+            "idx": i.idx,
+            "__checked": 1
+        })
+
+    update_child_qty_rate(doc.doctype,data, doc.name)
+@frappe.whitelist()
+def update_item_table(items, raw_materials):
+    data = json.loads(items)
+    materials = json.loads(raw_materials)
+
+    for i in data:
+        total = 0
+        for x in materials:
+            if 'service_item' in x:
+                if i['item_code'] == x['service_item']:
+                    total += x['amount']
+        frappe.db.sql(""" UPDATE `tabSales Order Item` SET rate=%s, amount=%s WHERE name=%s""",(total, total,i['name']))
+        frappe.db.commit()
+@frappe.whitelist()
+def update_totals(total, grand_total, rounded_total, name):
+    frappe.db.sql(""" UPDATE `tabSales Order` 
+                    SET total=%s, grand_total=%s, rounded_total=%s 
+                    WHERE name=%s""",(total, grand_total, rounded_total, name))
+    frappe.db.commit()
 @frappe.whitelist()
 def on_trash_so(doc, method):
     frappe.db.sql(""" UPDATE `tabCost Center` Set sales_order='' WHERE sales_order=%s""",doc.name)
--- spreads/public/js/sales_order.js
+++ spreads/public/js/sales_order.js
@@ -152,6 +152,157 @@ frappe.call({
     }
 })
 }
+erpnext.utils.update_raw_items = function(opts) {
+	const frm = opts.frm;
+	const cannot_add_row = (typeof opts.cannot_add_row === 'undefined') ? true : opts.cannot_add_row;
+	const child_docname = (typeof opts.cannot_add_row === 'undefined') ? "raw_material" : opts.child_docname;
+	const child_meta = frappe.get_meta("Raw Material");
+	console.log(child_meta)
+	const get_precision = (fieldname) => child_meta.fields.find(f => f.fieldname == fieldname).precision;
+
+	this.data = [];
+	const fields = [{
+		fieldtype:'Data',
+		fieldname:"docname",
+		read_only: 1,
+		hidden: 1,
+	},
+    {
+		fieldtype:'Link',
+		fieldname:"item_code",
+		options: 'Item',
+		in_list_view: 1,
+		read_only: 0,
+		disabled: 0,
+		label: __('Item Code'),
+		get_query: function() {
+			let filters;
+			if (frm.doc.doctype === 'Sales Order') {
+				filters = {"is_sales_item": 1};
+			} else if (frm.doc.doctype === 'Purchase Order') {
+				if (frm.doc.is_subcontracted === "Yes") {
+					filters = {"is_sub_contracted_item": 1};
+				} else {
+					filters = {"is_purchase_item": 1};
+				}
+			}
+			return {
+				query: "erpnext.controllers.queries.item_query",
+				filters: filters
+			};
+		}
+	},{
+		fieldtype:'Link',
+		options:'Item',
+		fieldname:"service_item",
+		in_list_view: 1,
+		label: __('Service Item'),
+        get_query: function() {
+            let filters;
+             var names = Array.from(cur_frm.doc.items, x => "item_code" in x ? x.item_code:"")
+
+                filters = [
+                   ["name", "in",names],
+                   ["assembled_item", "=",1],
+
+               ]
+            return {
+                query: "erpnext.controllers.queries.item_query",
+                filters: filters
+            };
+        }
+	},
+    {
+		fieldtype:'Float',
+		fieldname:"qty",
+		default: 0,
+		read_only: 0,
+		in_list_view: 1,
+		label: __('Qty'),
+		precision: get_precision("qty")
+	},
+        {
+		fieldtype:'Currency',
+		fieldname:"buying_price",
+		default: 0,
+		label: __('Buying Price'),
+	},
+        {
+		fieldtype:'Currency',
+		fieldname:"rate",
+		default: 0,
+		label: __('Selling Price'),
+            		in_list_view: 1,
+
+	},
+     {
+		fieldtype:'Link',
+		options:'Warehouse',
+		fieldname:"warehouse",
+		label: __('Warehouse'),
+
+	}
+];
+
+	const dialog = new frappe.ui.Dialog({
+		title: __("Update Items"),
+		fields: [
+			{
+				fieldname: "trans_items",
+				fieldtype: "Table",
+				label: "Items",
+				cannot_add_rows: cannot_add_row,
+				in_place_edit: false,
+				reqd: 1,
+				data: this.data,
+				get_data: () => {
+					return this.data;
+				},
+				fields: fields
+			},
+		],
+		primary_action: function() {
+			const trans_items = this.get_values()["trans_items"].filter((item) => !!item.item_code);
+			frappe.call({
+				method: 'spreads.doc_events.sales_order.update_raw_material',
+				freeze: true,
+				args: {
+					'parent_doctype': frm.doc.doctype,
+					'raw_material': trans_items,
+					'parent_doctype_name': frm.doc.name,
+					'child_docname': "raw_material"
+				},
+				callback: function() {
+					frm.reload_doc();
+				}
+			});
+			this.hide();
+			refresh_field("items");
+		},
+		primary_action_label: __('Update')
+	});
+
+	frm.doc['raw_material'].forEach(d => {
+	    console.log("RAW MATERIAL")
+	    console.log(d)
+		dialog.fields_dict.trans_items.df.data.push({
+			"docname": d.name,
+			"name": d.name,
+			"item_code": d.item_code,
+			"service_item": d.service_item,
+			"delivery_date": d.delivery_date,
+			"schedule_date": d.schedule_date,
+			"conversion_factor": d.conversion_factor,
+			"qty": d.qty,
+			"rate": d.rate,
+			"uom": d.uom
+		});
+		this.data = dialog.fields_dict.trans_items.df.data;
+		dialog.fields_dict.trans_items.grid.refresh();
+	})
+	dialog.show();
+}
+
 frappe.ui.form.on('Sales Order', {
 
     selling_price_list: function () {
@@ -174,6 +325,14 @@ frappe.ui.form.on('Sales Order', {
                ]
            }
        })
+        cur_frm.add_custom_button(__('Update Raw Materials'), () => {
+				erpnext.utils.update_raw_items({
+					frm: frm,
+					child_docname: "raw_material",
+					child_doctype: "Raw Material",
+					cannot_add_row: false,
+				})
+			});
 	}
 })
 cur_frm.cscript.update_available_stock = function () {
@@ -265,32 +424,34 @@ frappe.ui.form.on('Raw Material', {
 
     },
     warehouse: function (frm,cdt, cdn) {
-      var d = locals[cdt][cdn]
-    if(d.item_code && d.warehouse){
+        var d = locals[cdt][cdn]
+        if (d.item_code && d.warehouse) {
 
-        frappe.call({
-            method: "spreads.doc_events.quotation.get_rate",
-            args: {
-                item_code: d.item_code,
-                warehouse: d.warehouse ? d.warehouse : "",
-                based_on: cur_frm.doc.rate_of_materials_based_on ? cur_frm.doc.rate_of_materials_based_on : "",
-               price_list: cur_frm.doc.selling_price_list ? cur_frm.doc.selling_price_list : ""
+            frappe.call({
+                method: "spreads.doc_events.quotation.get_rate",
+                args: {
+                    item_code: d.item_code,
+                    warehouse: d.warehouse ? d.warehouse : "",
+                    based_on: cur_frm.doc.rate_of_materials_based_on ? cur_frm.doc.rate_of_materials_based_on : "",
+                    price_list: cur_frm.doc.selling_price_list ? cur_frm.doc.selling_price_list : ""
 
-            },
-            callback: function (r) {
-               d.buying_price = r.message[1]
+                },
+                callback: function (r) {
+                    d.buying_price = r.message[1]
                     d.rate = r.message[0]
                     d.amount = r.message[0] * d.qty
                     d.available_qty = r.message[2]
-                cur_frm.refresh_field("raw_material")
-                total_raw_material(cur_frm)
-            }
-        })
+                    cur_frm.refresh_field("raw_material")
+                    total_raw_material(cur_frm)
+                }
+            })
+        }
     }
-}
 })
+
 function update_service_item(cur_frm) {
     if(cur_frm.doc.docstatus === 0){
+        console.log("NAA DIRI")
         for (var x = 0; x < cur_frm.doc.items.length; x += 1) {
             var total = 0
             for (var y = 0; y < cur_frm.doc.raw_material.length; y += 1) {
@@ -309,17 +470,17 @@ function update_service_item(cur_frm) {
 function total_raw_material(cur_frm) {
     if(cur_frm.doc.docstatus === 0){
         var total = 0
-    var total_buying = 0
-    for(var x=0;x<cur_frm.doc.raw_material.length;x += 1){
-        total += cur_frm.doc.raw_material[x].amount
-        total_buying += (cur_frm.doc.raw_material[x].buying_price * cur_frm.doc.raw_material[x].qty)
-    }
-    cur_frm.doc.total_raw_material_expense = total
-    cur_frm.doc.total_raw_material_buying_expense = total_buying
-    cur_frm.doc.raw_material_profit = total - total_buying
-    cur_frm.refresh_fields(["total_raw_material_expense","total_raw_material_buying_expense", "raw_material_profit"])
-    total_expenses(cur_frm)
-    update_service_item(cur_frm)
+        var total_buying = 0
+        for(var x=0;x<cur_frm.doc.raw_material.length;x += 1){
+            total += cur_frm.doc.raw_material[x].amount
+            total_buying += (cur_frm.doc.raw_material[x].buying_price * cur_frm.doc.raw_material[x].qty)
+        }
+        cur_frm.doc.total_raw_material_expense = total
+        cur_frm.doc.total_raw_material_buying_expense = total_buying
+        cur_frm.doc.raw_material_profit = total - total_buying
+        cur_frm.refresh_fields(["total_raw_material_expense","total_raw_material_buying_expense", "raw_material_profit"])
+        total_expenses(cur_frm)
+        update_service_item(cur_frm)
     }
 
 }
