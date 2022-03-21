bc41e92f2797bfe9fee492468dc01b757e96f556
jiloysss <janlloydangeles@gmail.com>
2022-03-21 15:20:47 +0800

    Update

--- spreads/public/js/sales_order1.js
+++ spreads/public/js/sales_order1.js
@@ -1,3 +1,19 @@
+
+frappe.ui.form.on("Sales Order", {
+    refresh: function(frm, cdt, cdn) {
+        cur_frm.add_custom_button(__('MR for Raw Materials'), () => cur_frm.trigger("mr_material_request"), __('Create'));
+        // cur_frm.add_custom_button(__('Custom Sales Invoice'), () => cur_frm.trigger("make_sales_invoice"), __('Create'));
+        // cur_frm.add_custom_button(__('Custom Delivery Note'), () => cur_frm.trigger("make_delivery_note_based_on_delivery_date"), __('Create'));
+
+    },
+    mr_material_request: function() {
+		frappe.model.open_mapped_doc({
+			method: "spreads.doc_events.sales_order.make_mr",
+			frm: cur_frm
+		})
+    },
+})
+
 cur_frm.cscript.generate_project_id = function(){
     frappe.call({
         method: "spreads.doc_events.sales_order.generate_cc",
